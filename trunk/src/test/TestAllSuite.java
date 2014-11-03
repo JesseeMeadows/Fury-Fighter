eb32f4596e7c252bebb85daa4f5e7ca4077071ff
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-11-03 18:49:21 -0500

    Commit unit tests for FlyerModel.

--- trunk/src/test/TestAllSuite.java
+++ trunk/src/test/TestAllSuite.java
@@ -4,6 +4,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(value=org.junit.runners.Suite.class)
 @SuiteClasses(value={
 		TestMillisecTimer.class,
+		TestFlyerModel.class,
 
 })
 public class TestAllSuite {}
