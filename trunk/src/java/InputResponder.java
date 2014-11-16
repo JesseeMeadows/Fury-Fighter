b9b528444b908a7ada84b4dde4db6cb327a533f3
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-11-16 14:12:24 -0500

    Remove trunk and target.

--- trunk/src/java/InputResponder.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.KeyEvent;
-
-public interface InputResponder{
-	// refers to when a key is initially pressed down
-    public void keyDownResponse(KeyEvent e);
-    
-    // refers to when a pressed down key is released (think holding down fire key to constantly fire)
-    public void keyUpResponse(KeyEvent e);
-}
