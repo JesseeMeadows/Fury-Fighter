9ecfe2e5a3859809504a0b269b4a0db4e1c55914
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-11-24 14:46:02 -0500

    Finish Graphics2DMock.

--- src/test/Graphics2DMock.java
+++ src/test/Graphics2DMock.java
@@ -1,11 +1,34 @@
 import static org.mockito.Mockito.*;
+import org.mockito.ArgumentMatcher;
 import java.awt.Graphics2D;
+import java.text.AttributedCharacterIterator;
 
 public class Graphics2DMock
 {
+	private static class IsNullString extends ArgumentMatcher<String>
+	{
+		public boolean matches(Object a)
+		{
+			return a == null;
+		}
+	}
+
+	private static class IsNullAttributedCharacterIterator extends ArgumentMatcher<AttributedCharacterIterator>
+	{
+		public boolean matches(Object a)
+		{
+			return a == null;
+		}
+	}
+
 	public static Graphics2D getMockObject()
 	{
 		Graphics2D mock = mock(Graphics2D.class);
+
+		doThrow(new NullPointerException()).when(mock).drawString(argThat(new IsNullString()), any(), any());
+		doThrow(new NullPointerException()).when(mock).drawString(argThat(new IsNullAttributedCharacterIterator()), any(), any());
+		when(mock.drawImage(any(), any(), any())).thenReturn(true);
+
 		return mock;
 	}
 }
