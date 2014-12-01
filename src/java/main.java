4fef7bfcf4b12be32b38002294f37fd3c905f398
Aaron Disibio <disibioa1@nku073772.hh.nku.edu>
2014-12-01 11:32:53 -0500

    Fixed it for mac -- Aaron Disibio

--- src/java/main.java
+++ src/java/main.java
@@ -4,20 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Fury Fighter");
 
-		SoundManager sm = SoundManager.get();
-		sm.addSound("intro", "assets/snd/introjingle.wav");
-		sm.addSound("bullet", "assets/snd/bullet.wav");
-		sm.addSound("missle", "assets/snd/missle.wav");
-		sm.addSound("laser", "assets/snd/laser.wav");
-		sm.addSound("ring", "assets/snd/ring.wav");
-		sm.addSound("interface", "assets/snd/interface.wav");
-		sm.addSound("hit", "assets/snd/hit.wav");
-		sm.addSound("death", "assets/snd/death.wav");
-		sm.addSound("kill", "assets/snd/kill.wav");
-		sm.addSound("music", "assets/snd/music.wav");
-		sm.addSound("pickup", "assets/snd/pickup.wav");
-		sm.addSound("fragment", "assets/snd/fragment.wav");
-		sm.setLooping("music", true);
+
 
 		ModelController modelController = new ModelController(null);
 		ViewController viewController = new ViewController(modelController);
@@ -29,6 +16,22 @@ public class main {
 		//viewController.setMainView(new LevelView(viewController, "assets/test_level.png"));
 		 modelController.setMainModel(new SplashModel(modelController)); // Controller for Opening screen
 		 viewController.setMainView(new SplashView(viewController)); // View for opening screen
+        
+        
+        SoundManager sm = SoundManager.get();
+        sm.addSound("intro", "assets/snd/introjingle.wav");
+        sm.addSound("bullet", "assets/snd/bullet.wav");
+        sm.addSound("missle", "assets/snd/missle.wav");
+        sm.addSound("laser", "assets/snd/laser.wav");
+        sm.addSound("ring", "assets/snd/ring.wav");
+        sm.addSound("interface", "assets/snd/interface.wav");
+        sm.addSound("hit", "assets/snd/hit.wav");
+        sm.addSound("death", "assets/snd/death.wav");
+        sm.addSound("kill", "assets/snd/kill.wav");
+        sm.addSound("music", "assets/snd/music.wav");
+        sm.addSound("pickup", "assets/snd/pickup.wav");
+        sm.addSound("fragment", "assets/snd/fragment.wav");
+        sm.setLooping("music", true);
 
 		MillisecTimer timer = new MillisecTimer();
 		float MILLISEC_PER_FRAME = (float) 1000 / 30; // 30 FPS
